e693e819bf33e7e24e4093957c406d0ff6558705
andre trosky <andre@andre-mbp.bigpond>
2013-02-19 20:10:19 +1100

    Initial Commit

---
+++ databaseDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  databaseDemo
+//
+//  Created by andre trosky on 19/02/13.
+//  Copyright (c) 2013 andre trosky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ databaseDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  databaseDemo
+//
+//  Created by andre trosky on 19/02/13.
+//  Copyright (c) 2013 andre trosky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
